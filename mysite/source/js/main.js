d1e264d2014a7a6570089a87f57e213aa3b86ec9
Julian <juliantoker@gmail.com>
2012-10-24 23:09:55 -0400

    Configured fancybox and deleted unnecessary files.

--- mysite/source/js/main.js
+++ mysite/source/js/main.js
@@ -1,1 +1,15 @@
-
+$("#pic").click(function(event) {
+  //event.preventDefault();
+  $.fancybox({
+    padding: 0,
+    autoScale: true,
+    showCloseButton: true,
+    transitionIn: 'elastic',
+    transitionOut: 'fade',
+    width: 100,
+    height: 100,
+    type: 'image',
+    title: 'test',
+    href: "apple-touch-icon.png"
+  });
+});
